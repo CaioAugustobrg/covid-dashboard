1c3cb85a40bcbbbdcf22c61bdd994776545e296c
Caio Braga <caioaugustobrg@gmail.com>
2023-04-03 23:51:03 -0300

    :sparkles: feat: imported select and add it inside a div

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import '../src/style/App.css';
+import Select from 'react-select';
 
 function App() {
 
@@ -7,7 +8,12 @@ function App() {
 		<div className="App">
 			<h1>COVID 19 Dashboard</h1>
 			<div className='dashboard-container'>
-				<div className='dashboard-menu'></div>
+				<div className='dashboard-menu'>
+					<Select options={locationList}
+						className='dashboard-select'
+					/>
+         
+				</div>
 				<div className='dashboard-sumary'></div>
 			</div>
 		</div>
