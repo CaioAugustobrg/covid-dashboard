c113531459cf8766eb99135dd4fab7408417f9e7
Caio Braga <caioaugustobrg@gmail.com>
2023-04-04 13:27:23 -0300

    :bug: fix: there was 2 letters t in title

--- src/components/SumaryCard.jsx
+++ src/components/SumaryCard.jsx
@@ -1,10 +1,10 @@
-import React from "react";
+import React from 'react';
 
 export default function Card(props) {
-  return (
-    <div className="summary-card">
-      <h2>{props.tittle}</h2>
-      <p>{props.value}</p>
-    </div>
-  )
+	return (
+		<div className="summary-card">
+			<h2>{props.title}</h2>
+			<p>{props.value}</p>
+		</div>
+	);
 }
